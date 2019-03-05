f24c9e92a49d387df5cd1b11c444bea0d7c8ce2e
John Salisbury <jesalisb@usc.edu>
2019-03-04 23:24:21 -0800

    movie and datastore header

---
+++ hw4/movie.h
@@ -0,0 +1,21 @@
+#ifndef MOVIE_H
+#define MOVIE_H
+
+#include "product.h"
+
+class Movie : public Product {
+
+public: 
+	Movie(std::string name, double price, int qty, std::string rating,std::string genre);
+	std::set<std::string> keywords() const;
+	bool isMatch(std::vector<std::string>& searchTerms) const;
+	std::string displayString() const;
+	void dump(std::ostream& os) const;
+
+private:
+	std::string mGenre;
+	std::string mRating;
+	std::set<std::string> mKeywords;
+};
+
+#endif
